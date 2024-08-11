9358a88c36082572df42b0f5c7eb6aa1ac4d1cab
dimabond007 <dimistma@gmail.com>
2024-08-11 15:37:40 +0300

    login/reg pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import PageLayout from "./components/ui/PageLayout";
 import CleanersPage from "./pages/CleanersPage";
+import Login from "./pages/LoginPage";
+import Register from "./pages/RegisterPage";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
           <Route index element={<HomePage />} />
           <Route path="cleaners" element={<CleanersPage />} />
         </Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </div>
   );
