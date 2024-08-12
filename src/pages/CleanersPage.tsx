dfa6cc0835e152490edf18075d8d97375453172f
RonGamarnik <rgam2002@gmail.com>
2024-08-12 08:01:50 +0300

    8:01

--- src/pages/CleanersPage.tsx
+++ src/pages/CleanersPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import  { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { Star, LayoutGrid, List } from "lucide-react";
